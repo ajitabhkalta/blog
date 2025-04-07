af8ad0be130a078b0df82a3ea848e51df52bb8f9
ajitabhkalta <40548492+ajitabhkalta@users.noreply.github.com>
2025-04-07 07:42:59 +0000

    Add config changes

--- src/config.ts
+++ src/config.ts
@@ -12,9 +12,9 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/ajitabhkalta/blog/edit/",
   },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
